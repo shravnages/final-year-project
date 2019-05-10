edec15e88992da40f3d829cfe8608cd64e49fc87
Shravan Nageswaran <shravannageswaran@dyn3205-177.wlan.ic.ac.uk>
2019-05-10 18:42:07 +0100

    Added transfer that is static right now.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,6 @@ namespace App\Http\Controllers;
 
 use Auth;
 use Illuminate\Http\Request;
-use EthereumRPC\EthereumRPC;
-use ERC20\ERC20;
-use Web3\Web3;
 
 class HomeController extends Controller
 {
@@ -33,24 +30,10 @@ class HomeController extends Controller
         $transactions = Auth::user()->transactions;
         $id = Auth::user()->id;
 
-        //token
-        
-        /*$geth = new EthereumRPC('127.0.0.1', 8545);
-        $erc20 = new ERC20($geth);
-        $token = $erc20->token("0xf5e7f08c91b5d8579746eaad70ac509e94e2f1d3");
-        var_dump($token);*/
-
-        $web3 = new Web3('http://localhost:8545');
-        $eth = $web3->eth;
-
-        
-
-
         return view('home', [
             'balance' => $balance,
             'transactions' => $transactions,
             'id' => $id,
-            //'token_name' => $token_name
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,68 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main');
 
-        <title>Digipound</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
+@section('content');
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
@@ -80,8 +18,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Welcome to DigiPound
+                <div class="title m-b-md" id="welcome-message">
                 </div>
                 <div>
                     Stable Online Investments with Accurate Fiat Backing
@@ -94,5 +31,10 @@
                 </div>
             </div>
         </div>
+        <script>
+            token.name.call(function(err, name) {
+                document.getElementById("welcome-message").innerHTML = "Welcome to " + name;
+            });
+        </script>
     </body>
-</html>
+@endsection
