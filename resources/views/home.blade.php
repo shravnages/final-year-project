3c1ad8f39ae266205f004f94affe17327d73c410
Shravan Nageswaran <shravannageswaran@dyn3196-84.wlan.ic.ac.uk>
2019-05-21 10:23:11 +0100

    Small changes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard (ID: 0x{{ $account }})</div>
+                <div class="card-header">Dashboard (ID: {{ $id }} => 0x{{ $account }})</div>
 
                 <div class="card-body">
                     @if (session('status'))
